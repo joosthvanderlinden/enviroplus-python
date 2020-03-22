2b0ccaed84d748ecb548fdb0c942d8e6939a82e1
joosthvanderlinden <joosthvanderlinden@gmail.com>
2020-03-21 19:31:17 -0700

    test

--- analysis/main.py
+++ analysis/main.py
@@ -1,1 +1,14 @@
 # test
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+app = dash.Dash()
+
+app.layout = html.Div(children=[
+    html.H1(children='Dash Tutorials'),
+    dcc.Graph()
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
