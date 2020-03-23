f4fd8e69fa6aeacda84ebe5c7a8c002c2186270e
joosthvanderlinden <joosthvanderlinden@gmail.com>
2020-03-22 17:42:32 -0700

    first temperature graph

--- analysis/main.py
+++ analysis/main.py
@@ -1,14 +1,52 @@
-# test
 import dash
+from dash.dependencies import Output, Input
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly
+import random
+import plotly.graph_objs as go
+from collections import deque
 
-app = dash.Dash()
+from smbus2 import SMBus
+from bme280 import BME280
+bus = SMBus(1)
+bme280 = BME280(i2c_dev=bus)
 
-app.layout = html.Div(children=[
-    html.H1(children='Dash Tutorials'),
-    dcc.Graph()
-])
+
+
+X = deque(maxlen=20)
+X.append(25)
+Y = deque(maxlen=20)
+Y.append(1)
+
+
+app = dash.Dash(__name__)
+app.layout = html.Div(
+    [
+        dcc.Graph(id='live-graph', animate=True),
+        dcc.Interval(
+            id='graph-update',
+            interval=1*1000
+        ),
+    ]
+)
+
+@app.callback(Output('live-graph', 'figure'),
+              [Input('graph-update', 'n_intervals')])
+def update_graph_scatter(input_data):
+    X.append(X[-1]+1)
+    Y.append(bme280.get_temperature())
+
+    data = plotly.graph_objs.Scatter(
+            x=list(X),
+            y=list(Y),
+            name='Scatter',
+            mode= 'lines+markers'
+            )
+
+    return {'data': [data],'layout' : go.Layout(xaxis=dict(range=[min(X),max(X)]),
+                                                yaxis=dict(range=[min(Y),max(Y)]),)}
+
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(host='0.0.0.0', port=8080 ,debug=True)
