09ee37ee1c2ed9a476cad72cf6f81e192d127ed3
joosthvanderlinden <joosthvanderlinden@gmail.com>
2020-03-22 20:25:22 -0700

    bug fixes

--- analysis/main.py
+++ analysis/main.py
@@ -7,9 +7,12 @@ import random
 import plotly.graph_objs as go
 from collections import deque
 
-from smbus2 import SMBus
 from bme280 import BME280
-bus = SMBus(1)
+try:
+    from smbus2 import SMBus
+except ImportError:
+    from smbus import SMBus
+bus    = SMBus(1)
 bme280 = BME280(i2c_dev=bus)
 
 
