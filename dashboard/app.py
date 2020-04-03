af12c05246fe57156b20c06ec3c76b018b774869
joosthvanderlinden <joosthvanderlinden@gmail.com>
2020-04-02 19:41:45 -0700

    bug fix

--- dashboard/app.py
+++ dashboard/app.py
@@ -105,22 +105,11 @@ app.layout = html.Div([
 	html.Div([
         html.Div([
             html.Div([
-                html.H3(
-                    "Joost's Air Quality Dashboard",
-                    style={"margin-bottom": "0px"},
-                ),
-                html.H5(
-                    "Wouldn't cha know it.", 
-                    style={"margin-top": "0px"}
-                ),
-                html.Div(
-                	id='counter'
-                ),
-                html.Div(
-                	id='reset', 
-                	style={'display': 'none'}
-                ),
-                html.Button('Reset', id='button')]
+                html.H3("Joost's Air Quality Dashboard", style={"margin-bottom": "0px"}),
+                html.H5("Wouldn't cha know it.", style={"margin-top": "0px"}),
+                html.Div(id='reset', style={'display': 'none'}),
+                html.Button('Reset', id='button'),
+                html.Div(id='counter')]
             )],
             className="one-half column",
             id="title",
@@ -129,97 +118,6 @@ app.layout = html.Div([
 	    className="row flex-display",
 	    style={"margin-bottom": "25px"},
 	),
-
-	# html.Div([
-	#     html.Div([
- #            html.H3(
- #                "Air Quality Dashboard",
- #                style={"margin-bottom": "0px"},
- #            ),
- #            html.H5(
- #                "Wouldn't cha know it", 
- #                style={"margin-top": "0px"}
- #            )],
-	#         className="one-half column",
-	#         id="title",
-	#     ),
-	#     html.Div(id='counter')
-	#     ], 
-	#     id="header",
-	# 	className="row flex-display",
-	# 	style={"margin-bottom": "10px"},
- #    ),
-
-	# html.Div([
-	# 	html.Div(
- #            [html.H6(id="number-temperature-text"), html.P("Temperature")],
- #            id="number-temperature",
- #            className="number_container",
- #        ),
- #        html.Div(
- #            [html.H6(id="number-humidity-text"), html.P("Humidity")],
- #            id="number-humidity",
- #            className="number_container",
- #        ),
- #        html.Div(
- #            [html.H6(id="number-pressure-text"), html.P("Pressure")],
- #            id="number-pressure",
- #            className="number_container",
- #        ),
- #        html.Div(
- #            [html.H6(id="number-light-text"), html.P("Light")],
- #            id="number-light",
- #            className="number_container",
- #        ),
- #        html.Div(
- #            [html.H6(id="number-oxi-text"), html.P("OX*10")],
- #            id="number-oxi",
- #            className="number_container",
- #        ),
- #        html.Div(
- #            [html.H6(id="number-red-text"), html.P("RED")],
- #            id="number-red",
- #            className="number_container",
- #        ),
- #        html.Div(
- #            [html.H6(id="number-nh3-text"), html.P("NH3")],
- #            id="number-nh3",
- #            className="number_container",
- #        ),
- #        html.Div(
- #            [html.H6(id="number-pm03-text"), html.P(">0.3um")],
- #            id="number-pm03",
- #            className="number_container",
- #        ),
- #        html.Div(
- #            [html.H6(id="number-pm05-text"), html.P(">0.5um")],
- #            id="number-pm05",
- #            className="number_container",
- #        ),
- #        html.Div(
- #            [html.H6(id="number-pm10-text"), html.P(">1.0um")],
- #            id="number-pm10",
- #            className="number_container",
- #        ),
- #        html.Div(
- #            [html.H6(id="number-pm25-text"), html.P(">2.5um")],
- #            id="number-pm25",
- #            className="number_container",
- #        ),
- #        html.Div(
- #            [html.H6(id="number-pm50-text"), html.P(">5.0um")],
- #            id="number-pm50",
- #            className="number_container",
- #        ),
- #        html.Div(
- #            [html.H6(id="number-pm100-text"), html.P(">10.0um")],
- #            id="number-pm100",
- #            className="number_container",
- #        )], 
- #    	id="number-header", 
- #    	className="row container-display"
- #    ),
-
     html.Div([
 	    html.Div(
 	        [dcc.Graph(id='graph-temperature', animate=True)],
@@ -270,35 +168,27 @@ def unpack_values(Y):
 				Y_all.append(y)
 	return Y_all
 
-def round_values(Y):
-	rounded = []
-	for name, values in Y['values'].items():
-		v = values[-1]
-		if v is None:
-			rounded.append('')
-		else:
-			rounded.append(round(v, 1))
-	return tuple(rounded)
-
 # --------------------------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------------- RESET BUTTON
 @app.callback(Output('reset', 'children'),
 			  [Input('button', 'n_clicks')])
 def reset_data(n_clicks):
-	X.clear()
-	Y_temperature['values']['Temperature'].clear()
-	Y_humidity['values']['Humidity'].clear()
-	Y_pressure['values']['Pressure'].clear()
-	Y_light['values']['Light'].clear()
-	Y_gas['values']['OX*10'].clear()
-	Y_gas['values']['RED'].clear()
-	Y_gas['values']['NH3'].clear()
-	Y_pms['values']['>0.3um'].clear()
-	Y_pms['values']['>0.5um'].clear()
-	Y_pms['values']['>1.0um'].clear()
-	Y_pms['values']['>2.5um'].clear()
-	Y_pms['values']['>5.0um'].clear()
-	Y_pms['values']['>10.0um'].clear()
+	# On page reloads, n_clicks will be None
+	if n_clicks is not None:
+		X.clear()
+		Y_temperature['values']['Temperature'].clear()
+		Y_humidity['values']['Humidity'].clear()
+		Y_pressure['values']['Pressure'].clear()
+		Y_light['values']['Light'].clear()
+		Y_gas['values']['OX*10'].clear()
+		Y_gas['values']['RED'].clear()
+		Y_gas['values']['NH3'].clear()
+		Y_pms['values']['>0.3um'].clear()
+		Y_pms['values']['>0.5um'].clear()
+		Y_pms['values']['>1.0um'].clear()
+		Y_pms['values']['>2.5um'].clear()
+		Y_pms['values']['>5.0um'].clear()
+		Y_pms['values']['>10.0um'].clear()
 	return ''
 
 # --------------------------------------------------------------------------------------------------
@@ -320,26 +210,17 @@ def update_graph(X, Y):
 									yaxis_title=Y['units'],
 									xaxis=dict(range=[min(X),     max(X)]),
 									yaxis=dict(range=[min(Y_all), max(Y_all)]))
-	return (chart,)# + round_values(Y) # (chart,) creates single-item tuple
+	return chart
 
 # Time axis
 @app.callback(Output('counter', 'children'),
 			  [Input('graph-update', 'n_intervals')])
 def update_time(input_data):
 	X.append(datetime.now())
-	# if warmup:
-	# 	elapsed = (datetime.now() - start_time).total_seconds()
-	# 	if elapsed < warmup_time:
-	# 		return 'Warming up: {} seconds remaining'.format(warmup_time - elapsed)
-	# 	else:
-	# 		warmup = False
-	# else:
 	return 'Most recent update: {}'.format(X[-1])
 	
 # Temperature
-@app.callback([Output('graph-temperature', 'figure'),
-	 		   # Output('number-temperature-text', 'children')
-	 		   ],
+@app.callback(Output('graph-temperature', 'figure'),
 			  [Input('graph-update', 'n_intervals')])
 def update_graph_temperature(input_data):
 	try:
@@ -350,9 +231,7 @@ def update_graph_temperature(input_data):
 	return update_graph(X, Y_temperature)
 
 # Humidity
-@app.callback([Output('graph-humidity', 'figure'),
-	 		   # Output('number-humidity-text', 'children')
-	 		   ],
+@app.callback(Output('graph-humidity', 'figure'),
 			  [Input('graph-update', 'n_intervals')])
 def update_graph_temperature(input_data):
 	try:
@@ -363,9 +242,7 @@ def update_graph_temperature(input_data):
 	return update_graph(X, Y_humidity)
 
 # Pressure
-@app.callback([Output('graph-pressure', 'figure'),
-	 		   # Output('number-pressure-text', 'children')
-	 		   ],
+@app.callback(Output('graph-pressure', 'figure'),
 			  [Input('graph-update', 'n_intervals')])
 def update_graph_temperature(input_data):
 	try:
@@ -376,9 +253,7 @@ def update_graph_temperature(input_data):
 	return update_graph(X, Y_pressure)
 
 # Light
-@app.callback([Output('graph-light', 'figure'),
-	 		   # Output('number-light-text', 'children')
-	 		   ],
+@app.callback(Output('graph-light', 'figure'),
 			  [Input('graph-update', 'n_intervals')])
 def update_graph_temperature(input_data):
 	try:
@@ -389,11 +264,7 @@ def update_graph_temperature(input_data):
 	return update_graph(X, Y_light)
 
 # Gases
-@app.callback([Output('graph-gases', 'figure'),
-	 		   # Output('number-oxi-text', 'children'),
-	 		   # Output('number-red-text', 'children'),
-	 		   # Output('number-nh3-text', 'children')
-	 		   ],
+@app.callback(Output('graph-gases', 'figure'),
 			  [Input('graph-update', 'n_intervals')])
 def update_graph_gases(input_data):
 	try:
@@ -411,14 +282,7 @@ def update_graph_gases(input_data):
 	return update_graph(X, Y_gas)
 
 # Particulate matter
-@app.callback([Output('graph-particulates', 'figure'),
-	 		   # Output('number-pm03-text', 'children'),
-	 		   # Output('number-pm05-text', 'children'),
-	 		   # Output('number-pm10-text', 'children'),
-	 		   # Output('number-pm25-text', 'children'),
-	 		   # Output('number-pm50-text', 'children'),
-	 		   # Output('number-pm100-text', 'children')
-	 		   ],
+@app.callback(Output('graph-particulates', 'figure'),
 			  [Input('graph-update', 'n_intervals')])
 def update_graph_particulates(input_data):
 	try:
